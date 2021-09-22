cd2f5ae2be84160200f7ab7bc66459fb330f9f55
sll <evol.simon@gmail.com>
2021-09-22 13:09:28 -0400

    added mapping

--- scripts/abc.r
+++ scripts/abc.r
@@ -2,29 +2,7 @@ library("arrow")
 library("abc")
 library("optparse")
 
-method = "loclinear"
-transf = "none"
-hcorr  = "TRUE"
-kernel = "epanechnikov"
-
-option_list = list(
-    make_option("--reg_model", type="character", default=NULL,help="--reg_model", metavar="character"),
-    make_option("--hcorr", type="character", default=NULL,  help="--hcorr", metavar="character"),
-    make_option("--kernel", type="character", default=NULL, help="--kernel", metavar="character"),
-    make_option("--transf", type="character", default=NULL, help="--transf", metavar="character"),
-    make_option("--df_true_ss", type="character", default=NULL, help="--df_true_ss", metavar="character"),
-    make_option("--df_simu_space_knn_params", type="character", default=NULL,  help="--df_simu_space_knn_params", metavar="character"),
-    make_option("--df_simu_space_knn_ss", type="character", default=NULL, help="--df_simu_space_knn_ss", metavar="character"),
-    make_option("--output_dir", type="character", default=NULL, help="--output_dir", metavar="character")
-)
- 
-opt_parser = OptionParser(option_list=option_list)
-opt = parse_args(opt_parser)
-
-if (is.null(opt$reg_model)){
-  print_help(opt_parser)
-   stop("", call.=FALSE)
-}
+wd <- "/data/" #paste0(getwd(),"/")
 
 main <- function(opt){
     df_knn <- abc(
@@ -37,14 +15,38 @@ main <- function(opt){
         hcorr = opt$hcorr, 
         kernel = opt$kernel, 
         sizenet = 1)$adj.values
-
-    write_feather(x=as.data.frame(df_knn),sink = paste0("/data/", "df_knn.feather",version = 2))
+    print(getwd())
+    write_feather(x=as.data.frame(df_knn), sink=paste0(wd, "df_knn.feather",version = 2))
 }
+
 
 read_f <- function(filename) {
-
-    df <- as.data.frame(read_feather(paste0("/data/", filename)))
+    df <- as.data.frame(read_feather(paste0(wd, filename)))
+    print(dim(df))
     return(df)
-
 }
+
+
+option_list = list(
+    make_option("--reg_model", type="character", default=NULL,help="--reg_model", metavar="character"),
+    make_option("--hcorr", type="character", default=NULL,  help="--hcorr", metavar="character"),
+    make_option("--kernel", type="character", default=NULL, help="--kernel", metavar="character"),
+    make_option("--transf", type="character", default=NULL, help="--transf", metavar="character"),
+    make_option("--df_true_ss", type="character", default=NULL, help="--df_true_ss", metavar="character"),
+    make_option("--df_simu_space_knn_params", type="character", default=NULL,  help="--df_simu_space_knn_params", metavar="character"),
+    make_option("--df_simu_space_knn_ss", type="character", default=NULL, help="--df_simu_space_knn_ss", metavar="character")
+)
+opt_parser = OptionParser(option_list=option_list)
+opt = parse_args(opt_parser)
+
+if (is.null(opt$reg_model)){
+  print_help(opt_parser)
+   stop("", call.=FALSE)
+}
+
+main(opt=opt)
+
+
+
+
 
